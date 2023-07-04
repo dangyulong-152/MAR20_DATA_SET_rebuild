5ec21d04f7ab3f23b38dbb4cd796704e211bc95c
dangyulong_ordos <78517801+dangyulong-152@users.noreply.github.com>
2023-07-04 22:27:06 +0800

    Create ReadME.py

---
+++ ReadME.py
@@ -0,0 +1,9 @@
+MAR20-COCO_format-HH:
+链接：https://pan.baidu.com/s/1jaT19MfubIgwnSnrlQtNKQ?pwd=2jtx 
+提取码：2jtx 
+--来自百度网盘超级会员V5的分享
+
+MAR20-DOTA-FORMAT-OBB:
+链接：https://pan.baidu.com/s/1jEyzxvUhcmGmQZXBcBOpqw?pwd=mlbm 
+提取码：mlbm 
+--来自百度网盘超级会员V5的分享
